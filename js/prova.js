797def1a73ebb5b62466ff179ac4195c80a1644d
MariaJyate <maria.jyate@gmail.com>
2024-02-07 00:32:49 +0100

    recreating design from scratch bc life is meaningless

--- js/prova.js
+++ js/prova.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { mergeVertices } from 'three/examples/jsm/utils/BufferGeometryUtils';
+
 
 const scene = new THREE.Scene();
 scene.background = null;
@@ -22,21 +24,32 @@ const controls = new OrbitControls(camera, renderer.domElement);
 
 
 
-// const ambientLight = new THREE.AmbientLight( 0xffffff, 1000);
-// ambientLight.position.set(0,2,0);
-// scene.add( ambientLight );
+const ambientLight = new THREE.AmbientLight( 0xffffff, 100);
+ambientLight.position.set(0,2,0);
+scene.add( ambientLight );
 
-const axesHelper = new THREE.AxesHelper( 5 );
-scene.add( axesHelper );
+const directionalLight = new THREE.DirectionalLight( 0x00ffff, 50);
+directionalLight.position.set(10,-10,-10);
+scene.add(directionalLight);
 
-const geometry = new THREE.SphereGeometry();
+
+
+const geometry = new THREE.SphereGeometry(1.2 , 64);
+
 
 const material = new THREE.MeshPhysicalMaterial({
     color: 0x4287f5,
     specularColor: 0xffffff,
+    transparent: true,
+    opacity: 0.2,
+    emissive: 0xc646c8,
     transmission: 1,
     thickness: 1,
     roughness: 0,
+    metalness: 0.5,
+    reflectivity: 0.5,
+    ior: 2.3,
+    // wireframe: true,
     iridescence: 1,
     iridescenceIOR: 1.0,
 
@@ -47,6 +60,38 @@ const mesh = new THREE.Mesh(geometry, material);
 mesh.position.set(0,0,0);
 
 scene.add(mesh);
+
+const geometry2 = new THREE.SphereGeometry(0.15);
+
+const material2 = new THREE.MeshPhysicalMaterial({
+    color: 0xf252c5,
+    roughness: 1,
+    opacity:1,
+    ior: 0.5,
+    reflectivity:0.5,
+});
+
+const mesh2 = new THREE.Mesh(geometry2, material2);
+mesh2.position.set(0,0,0);
+
+scene.add(mesh2);
+
+const geometry3 = new THREE.TorusKnotGeometry(0.65, 0.15, 64, 8, 2, 3);
+const material3 = new THREE.MeshPhysicalMaterial({
+    color: 0x211c1f,
+    roughness:0,
+    opacity:1,
+    ior: 0.5,
+    reflectivity: 0,
+    metalness: 0
+});
+
+const mesh3 = new THREE.Mesh(geometry3, material3);
+mesh3.position.set(0,0,0);
+
+scene.add(mesh3);
+
+
 
 renderer.render(scene, camera);
 
