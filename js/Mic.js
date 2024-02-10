dcb81aa24f9e027cdef806a38932341ee5374fc9
MariaJyate <maria.jyate@gmail.com>
2024-02-10 16:21:24 +0100

    mic

--- js/Mic.js
+++ js/Mic.js
@@ -1,6 +1,67 @@
 export default class Microphone{
 
     constructor(){
-        console.log('mic');
+
+        this.ready = false;
+        this.volume = 0;
+
+        navigator.mediaDevices
+            .getUserMedia({ audio : true, video: false})
+            .then((_stream) => {
+
+                this.stream = _stream
+                // console.log('mic');
+                // console.log(_stream);
+
+                this.init()
+                this.update();
+
+            })
+        
+
+        // const pcmData = new Float32Array(analyserNode.fftSize);
+        // const onFrame = () => {
+        //     analyserNode.getFloatTimeDomainData(pcmData);
+        //     let sumSquares = 0.0;
+        //     for (const amplitude of pcmData) {sumSquares += amplitude * amplitude ; }
+        //     volumeMeterEl.value = Math.sqrt(sumSquares / pcmData.lenght);
+        //     window.requestAnimationFrame(onFrame)
+        // };
+
+        // window.requestAnimationFrame(onFrame);
+        
+    }
+
+    init(){
+        this.audioContext = new AudioContext();
+        
+        this.mediaStreamSourceNode = this.audioContext.createMediaStreamSource(this.stream);
+        
+        
+        this.analyserNode = this.audioContext.createAnalyser();
+        this.analyserNode.fftSize = 128;
+        
+        this.mediaStreamSourceNode.connect(this.analyserNode);
+        
+        this.pcmData = new Float32Array(this.analyserNode.fftSize);
+        console.log(this.analyserNode.fftSize);
+        
+        this.ready = true;
+    }
+
+    update(){
+
+        if(!this.ready)
+            return
+
+        this.analyserNode.getFloatTimeDomainData(this.pcmData);
+        console.log(this.pcmData);
+
+        let sumSquares = 0.0;
+        for (const amplitude of this.pcmData) {
+            sumSquares += amplitude * amplitude
+        }
+        this.volume = Math.sqrt(sumSquares / this.pcmData.lenght);
+        console.log(volume);
     }
 }
